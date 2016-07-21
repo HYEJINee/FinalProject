bd1aab33ec22f962086a1a9716529467307e583b
Seo Jun Ho <stjwhg44@naver.com>
2016-07-21 14:40:13 +0900

    read 로그인 전 후일부 하는중

--- src/main/java/com/four/myapp/controller/ReadController.java
+++ src/main/java/com/four/myapp/controller/ReadController.java
@@ -3,6 +3,8 @@ package com.four.myapp.controller;
 
 import java.sql.SQLException;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.four.myapp.domain.MemberVO;
 import com.four.myapp.service.ReadService;
 
 /**
@@ -21,7 +24,11 @@ public class ReadController {
 	@Autowired
 	private ReadService service;
 	@RequestMapping(value="read", method=RequestMethod.GET)
-	public void readget(@RequestParam("topic_no") int topic_no, Model model) throws SQLException {
+	public void readget(@RequestParam("topic_no") int topic_no, Model model, HttpSession session) throws SQLException {
+		MemberVO vo = (MemberVO)session.getAttribute("USER_KEY");
+		if(vo != null) {
+			model.addAttribute("readuser", vo);
+	      }
 		model.addAttribute("readlist",service.Readdao(topic_no));
 		model.addAttribute("readResource",service.getResource(topic_no));
 		model.addAttribute("readOpinion",service.getOpinion(topic_no));
