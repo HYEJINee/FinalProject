532d4df3fb0bd27290093fe463807878854101fc
Horyun Song <horyunsong@gmail.com>
2016-08-01 14:45:29 +0900

    integrate master to noti

---
+++ src/main/java/com/four/myapp/persistence/MypageDAO.java
@@ -0,0 +1,19 @@
+package com.four.myapp.persistence;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import com.four.myapp.domain.MainDto;
+import com.four.myapp.domain.MemberVO;
+import com.four.myapp.domain.TimelineDTO;
+
+public interface MypageDAO {
+	public List<TimelineDTO> timeline(String user_no) throws SQLException;
+	public List<TimelineDTO> getNoti(String user_no) throws SQLException;
+	public void dismissNoti(String noti_no) throws SQLException;
+	public List<MainDto> getMyList(String user_no) throws SQLException;
+	public List<MainDto> getMyRecmdList(String user_no) throws SQLException;
+	public List<MainDto> getMyFinishList(String user_no) throws SQLException;
+	public void uploadProfile(MemberVO memberVO);
+
+}
---
+++ src/main/java/com/four/myapp/persistence/MypageDAOImpl.java
@@ -0,0 +1,58 @@
+package com.four.myapp.persistence;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.stereotype.Repository;
+
+import com.four.myapp.domain.MainDto;
+import com.four.myapp.domain.MemberVO;
+import com.four.myapp.domain.TimelineDTO;
+
+@Repository
+public class MypageDAOImpl implements MypageDAO {
+	@Inject
+	private SqlSession sqlSession;
+
+	private static final String NAMESPACE = "com.four.mappers.mypageMapper";
+
+	@Override
+	public List<TimelineDTO> timeline(String user_no) throws SQLException {
+		return sqlSession.selectList(NAMESPACE + ".timeline", user_no);
+	}
+
+	@Override
+	public List<TimelineDTO> getNoti(String user_no) throws SQLException {
+		return sqlSession.selectList(NAMESPACE + ".getNoti", user_no);
+	}
+
+	@Override
+	public void dismissNoti(String noti_no) throws SQLException {
+		sqlSession.update(NAMESPACE + ".dismissNoti", noti_no);
+	}
+
+	@Override
+	public List<MainDto> getMyList(String user_no) throws SQLException {
+		return sqlSession.selectList(NAMESPACE + ".getMyList", user_no);
+	}
+
+	@Override
+	public List<MainDto> getMyRecmdList(String user_no) throws SQLException {
+		return sqlSession.selectList(NAMESPACE + ".getMyRecmdList", user_no);
+	}
+
+	@Override
+	public List<MainDto> getMyFinishList(String user_no) throws SQLException {
+		return sqlSession.selectList(NAMESPACE + ".getMyFinishList", user_no);
+
+	}
+
+	@Override
+	public void uploadProfile(MemberVO memberVO) {
+		sqlSession.update(NAMESPACE + ".uploadProfile", memberVO);
+	}
+
+}
