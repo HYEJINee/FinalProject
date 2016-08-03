2b6c926900336df7d832c0309fc19e8a08c85e09
Horyun Song <horyunsong@gmail.com>
2016-08-03 15:08:22 +0900

    모든 timeline noti 되긴됨 - 마이페이지에서 수정 필요

--- src/main/java/com/four/myapp/util/ExpiredTopicUpdater.java
+++ src/main/java/com/four/myapp/util/ExpiredTopicUpdater.java
@@ -1,5 +1,8 @@
 package com.four.myapp.util;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.ibatis.session.SqlSession;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
@@ -9,6 +12,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.four.myapp.domain.NotiDTO;
+import com.four.myapp.domain.TimelineDTO;
+
 @Aspect
 public class ExpiredTopicUpdater {
 	@Autowired
@@ -16,6 +22,12 @@ public class ExpiredTopicUpdater {
 	Logger logger = LoggerFactory.getLogger(ExpiredTopicUpdater.class);
 	
 	private static final String NAMESPACE = "com.four.myapp.mappers.expiredMapper";
+	private static final String timelineNAMESPACE = "com.four.myapp.mappers.timelineMapper";
+	
+	private List<Integer> failed = new ArrayList<Integer>();
+	private List<Integer> finished = new ArrayList<Integer>();
+	NotiDTO notiDTO = new NotiDTO();
+	TimelineDTO timelineDTO = new TimelineDTO();
 	
 	@Pointcut("bean(*DAOImpl)")
 	private void targetMethod() { }
@@ -24,14 +36,47 @@ public class ExpiredTopicUpdater {
 	public void beforeTragetMethod(JoinPoint joinPoint) {
 		logger.info("AspectUsingAnnotation.beforeTargetMethod executed.");
 		
-		if(sqlSession.selectList(NAMESPACE + ".checkExpired2").isEmpty() == false) {
-			sqlSession.update(NAMESPACE + ".expiredProposal2");
-        	logger.info("expired proposal updated.");
-        }
-		
         if(sqlSession.selectList(NAMESPACE + ".checkExpired").isEmpty() == false) {
+        	failed = sqlSession.selectList(NAMESPACE + ".checkExpired");
+        	
+        	//Timeline-Noti ("2") update
+        	for(int i=0; i<failed.size(); i++) {
+        		int topic_no = failed.get(i);
+        		timelineDTO = sqlSession.selectOne(NAMESPACE + ".getTimelineDTO", topic_no);
+        		timelineDTO.setTimeline_type("2");
+        		System.out.println(timelineDTO.toString());
+        		sqlSession.insert(NAMESPACE + ".timelineTopic", timelineDTO);
+        		
+        		notiDTO.setUser_no(timelineDTO.getUser_no());
+        		notiDTO.setTopic_no(topic_no);
+        		notiDTO.setNoti_type("2");
+        		sqlSession.insert(NAMESPACE + ".notiTopic", notiDTO);
+        	}
+        	
         	sqlSession.update(NAMESPACE + ".expiredProposal");
         	logger.info("expired proposal updated.");
         }
+		
+		if(sqlSession.selectList(NAMESPACE + ".checkExpired2").isEmpty() == false) {
+			failed = sqlSession.selectList(NAMESPACE + ".checkExpired2");
+        	
+        	//Timeline-Noti ("4") update
+        	for(int i=0; i<failed.size(); i++) {
+        		int topic_no = failed.get(i);
+        		timelineDTO = sqlSession.selectOne(NAMESPACE + ".getTimelineDTO", topic_no);
+        		timelineDTO.setTimeline_type("4");
+        		System.out.println(timelineDTO.toString());
+        		sqlSession.insert(NAMESPACE + ".timelineTopic", timelineDTO);
+        		
+        		notiDTO.setUser_no(timelineDTO.getUser_no());
+        		notiDTO.setTopic_no(topic_no);
+        		notiDTO.setNoti_type("4");
+        		sqlSession.insert(NAMESPACE + ".notiTopic", notiDTO);
+        	}
+        	
+        	sqlSession.update(NAMESPACE + ".expiredProposal2");
+        	logger.info("expired proposal updated.");
+        }
+
 	}
 }
