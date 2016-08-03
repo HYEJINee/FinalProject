40312618346bcc7be90cebeecac0ff5f58fdcf9d
bbuing <chlfkrgnl@naver.com>
2016-08-03 12:05:35 +0900

    merge from ongoing

---
+++ src/main/java/com/four/myapp/controller/FinishedController.java
@@ -0,0 +1,66 @@
+package com.four.myapp.controller;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.four.myapp.domain.MainDto;
+import com.four.myapp.domain.MemberVO;
+import com.four.myapp.service.FinishedService;
+import com.four.myapp.service.ReadService;
+
+@Controller
+@RequestMapping("/finished/*")
+public class FinishedController {
+	@Autowired
+	private FinishedService service;
+	
+	@RequestMapping("/list")
+	public void FinishedHandler(@RequestParam(required=false) Integer pageNo, Model model) throws SQLException {
+		if(pageNo == null)
+			  pageNo = 1;
+		int index = (pageNo-1) * 9;
+		
+		model.addAttribute("list", service.getAll_fin(index));
+	}
+	
+	@RequestMapping("/tab")
+	public @ResponseBody List<MainDto> TabHandler(@RequestParam("pageNo") Integer pageNo, @RequestParam("type") String type, Model model) throws SQLException {
+		int index = 0;
+		if(pageNo == null)
+			  pageNo = 1;
+		index = (pageNo-1) * 9;
+		
+		if(type.equals("all"))
+			return service.getAll_fin(index);
+		else if(type.equals("pro-con"))
+			return service.getProCon_fin(index);
+		else
+			return service.getFree_fin(index);
+	}
+	
+	@Autowired
+	private ReadService read_service;
+	
+	@RequestMapping(value="/read", method=RequestMethod.GET)
+	public void readget(@RequestParam(required=false) Integer pageNo, @RequestParam("topic_no") int topic_no, Model model, HttpSession session) throws SQLException {
+		MemberVO vo = (MemberVO)session.getAttribute("USER_KEY");
+		if(vo != null) {
+			model.addAttribute("readuser", vo);
+	      }
+		if(pageNo == null) { pageNo = 1; }
+		model.addAttribute("readlist",read_service.Readdao(topic_no));
+		model.addAttribute("readResource",read_service.getResource(topic_no));
+		model.addAttribute("readOpinion",read_service.getOpinion(topic_no, pageNo));
+		model.addAttribute("taglist",read_service.getTaglist(topic_no));
+	}
+}
---
+++ src/main/webapp/resources/finished/js/finished.js
@@ -0,0 +1,57 @@
+/**
+ * 	finished.js
+ * 	작성자 : 최락휘
+ * 	작성일 : 2016.07.21
+ *  최종수정 : 2016.08.01
+ *  내용 : 전체 헤더 탭 활성화, 카테고리 기능 
+ */
+// 게시물 선택시 읽기 페이지로 이동
+function fnRead() {
+	$(".item").click(function(){
+		var topic_no = $(this).children().filter("input").val();
+		location.href = "/finished/read?topic_no=" + topic_no;
+	});
+}
+// 무한스크롤 설정
+function fnInfiScroll() {
+	var $grid = $('.list');
+		
+		$grid.isotope({
+			// set itemSelector so .grid-sizer is not used in layout
+			itemSelector: '.item',
+			// use element for option
+			masonry: {
+				gutter: 50
+			}
+		});
+				
+		$grid.infinitescroll({
+			navSelector : '#page_nav',
+			nextSelector : '#page_nav p a',
+			itemSelector : '.item',
+			loading: {
+				selector: '#load',
+			    finishedMsg: "<em>모든 안건을 불러왔습니다.</em>",
+			                img: 'http://i.imgur.com/qkKy8.gif',
+			    msgText: "<em>안건을 불러오는 중...</em>"
+			}
+		},
+			function(newElements) {
+				$grid.isotope('appended', $(newElements));
+			}
+		);
+		
+		$('.nav-pills').on('click', 'li', function() {
+			var filterValue = $(this).attr('data-filter');
+			$grid.isotope({filter: filterValue});
+		})
+	}
+
+$(document).ready(function(){
+	fnInfiScroll();
+	fnRead();
+	// 헤더 탭 부분 종료된 토론 활성화
+	$("#proposal").removeClass("active");
+	$("#ongoing").removeClass("active");
+	$("#finished").addClass("active");
+});
